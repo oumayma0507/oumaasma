4f467fd5fd9de630ba483164c77f557ea494359a
oumaima bouzayen <maymabouzayenne@gmail.com>
2026-05-22 01:18:43 +0200

    Allow production build with lint warnings

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
